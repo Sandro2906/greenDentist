a5ce94050ad1342f0f39e206c6bf466d0587c6c2
Sandro Gataric <132219558+Sandro2906@users.noreply.github.com>
2023-09-22 21:22:34 +0200

    Update NavBar.jsx

--- frontend/src/pages/NavBar.jsx
+++ frontend/src/pages/NavBar.jsx
@@ -60,7 +60,7 @@ const NavBar = () => {
                 </svg>
         </div>
        
-        <div className='w-[400px] hidden h-16 justify-center items-center bg-emerald-300 font-semibold text-xl text-blue-700 lg:flex'><svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
+        <div className='w-[400px] hidden h-16 justify-center items-center bg-emerald-300 font-semibold text-xl text-white lg:flex'><svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
              <path strokeLinecap="round" strokeLinejoin="round" d="M10.5 1.5H8.25A2.25 2.25 0 006 3.75v16.5a2.25 2.25 0 002.25 2.25h7.5A2.25 2.25 0 0018 20.25V3.75a2.25 2.25 0 00-2.25-2.25H13.5m-3 0V3h3V1.5m-3 0h3m-3 18.75h3" />
              </svg>
              <h1>066/889-435</h1>
@@ -86,10 +86,10 @@ const NavBar = () => {
                               
                         ))
                     }
-                    <div className='w-[200px] h-20 realtive flex justify-center items-center  bg-blue-300 font-semibold text-xl text-blue-700 '><svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
+                    <div className='w-[200px] h-20 realtive flex justify-center items-center  bg-emerald-300 font-semibold text-xl text-white '><svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
              <path strokeLinecap="round" strokeLinejoin="round" d="M10.5 1.5H8.25A2.25 2.25 0 006 3.75v16.5a2.25 2.25 0 002.25 2.25h7.5A2.25 2.25 0 0018 20.25V3.75a2.25 2.25 0 00-2.25-2.25H13.5m-3 0V3h3V1.5m-3 0h3m-3 18.75h3" />
              </svg>
-             <h1>(561) 318-8762</h1>
+             <h1>066/889-435</h1>
             </div>
 
                     </div>
